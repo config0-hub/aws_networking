7a31d8d71c5d8bb8d8b782a8a6dd9c3dcc66ea05
gary <gary>
2025-04-16 21:04:11 +0800

    'update'

--- execgroups/_config0_configs/vpc_simple/_actions/30-exec_terraform.py
+++ execgroups/_config0_configs/vpc_simple/_actions/30-exec_terraform.py
@@ -3,6 +3,6 @@ def default():
         'method': 'shelloutconfig',
         'metadata': {
             'shelloutconfigs': ['config0-publish:::terraform::resource_wrapper'],
-            'env_vars': ['config0-publish:::docker::build.env']
+            'env_vars': ['config0-publish:::docker::build']
         }
     }
