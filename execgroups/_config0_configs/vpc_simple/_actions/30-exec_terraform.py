b9a12183185d2d3a006d52e7427bb2aa2579e5b8
gary <gary>
2025-04-17 12:16:24 +0800

    'update'

---
+++ execgroups/_config0_configs/vpc_simple/_actions/30-exec_terraform.py
@@ -0,0 +1,8 @@
+def default():
+    return {
+        'method': 'shelloutconfig',
+        'metadata': {
+            'shelloutconfigs': ['config0-publish:::terraform::resource_wrapper'],
+            'env_vars': ['config0-publish:::docker::build']
+        }
+    }
---
+++ execgroups/_config0_configs/vpc_simple/_chrootfiles/var/tmp/terraform/variables.tf
@@ -0,0 +1,46 @@
+variable "aws_default_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "vpc name"
+  default     = "default_vpc_name_config0"
+}
+
+variable "vpc_tags" {
+  description = "VPC resource tags"
+  type        = map(string)
+  default     = {}
+}
+
+variable "public_subnet_tags" {
+  default     = { "kubernetes.io/role/elb" : "1" }
+  description = "public subnet tags"
+  type        = map(string)
+}
+
+variable "private_subnet_tags" {
+  default     = { "kubernetes.io/role/internal_elb" : "1" }
+  description = "private subnet tags"
+  type        = map(string)
+}
+
+variable "cloud_tags" {
+  description = "additional tags as a map"
+  type        = map(string)
+  default     = {}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
