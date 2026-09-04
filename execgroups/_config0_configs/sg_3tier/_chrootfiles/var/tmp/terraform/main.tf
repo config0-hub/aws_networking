201b747baec668b7a6186a80fb78038848b662c5
Gary <gwleong@gmail.com>
2026-09-04 13:05:01 -0400

    rendered and updated 09-04-2026-13-04

--- execgroups/_config0_configs/sg_3tier/_chrootfiles/var/tmp/terraform/main.tf
+++ execgroups/_config0_configs/sg_3tier/_chrootfiles/var/tmp/terraform/main.tf
@@ -29,6 +29,22 @@ resource "aws_security_group" "bastion" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow all TCP traffic within bastion security group"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "Allow all UDP traffic within bastion security group"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
+    self        = true
+  }
+
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
@@ -75,6 +91,22 @@ resource "aws_security_group" "web" {
     security_groups = [aws_security_group.bastion.id]
   }
 
+  ingress {
+    description = "Allow all TCP traffic within web security group"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "Allow all UDP traffic within web security group"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
+    self        = true
+  }
+
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
@@ -111,6 +143,22 @@ resource "aws_security_group" "api" {
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id]
+  }
+
+  ingress {
+    description = "Allow all TCP traffic within API security group"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "Allow all UDP traffic within API security group"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
+    self        = true
   }
 
   egress {
@@ -151,6 +199,22 @@ resource "aws_security_group" "database" {
     security_groups = [aws_security_group.bastion.id]
   }
 
+  ingress {
+    description = "Allow all TCP traffic within database security group"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    description = "Allow all UDP traffic within database security group"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
+    self        = true
+  }
+
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
@@ -167,4 +231,3 @@ resource "aws_security_group" "database" {
     },
   )
 }
-
